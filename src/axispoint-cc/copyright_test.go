c7a28cd38f082c5e315d85d3e5224c10da07f2ab
vandan.gogna@ibm.com <vandan.gogna@ibm.com>
2018-12-05 15:51:59 -0500

    unit test update.

--- src/axispoint-cc/copyright_test.go
+++ src/axispoint-cc/copyright_test.go
@@ -12,6 +12,7 @@ const copyrightDataReportUUID = "1cfbdb47-cca7-3eca-b73e-0d6c478a5abc"
 //var copyrightDataReportSingleInput = `[{"one":"1","two":"2","three":"3","docType":"COPYRIGHTDATAREPORT","copyrightDataReportUUID":"1cfbdb47-cca7-3eca-b73e-0d6c478a4eaa"}]`
 var copyrightDataReportSingleInput = `[{"copyrightDataReportUUID":"1cfbdb47-cca7-3eca-b73e-0d6c478a5abc","docType":"COPYRIGHTDATAREPORT","isrc":"123Src","songTitle":"NY NY","startDate":"2018-01-01T21:17:34.371Z","endDate":"2018-11-15T22:27:34.111Z","rightHolders":[{"ipi":"ipi1","percent":42},{"ipi":"ipi2","percent":33}]}]`
 var copyrightDataReportSingleOutput = `{"docType":"COPYRIGHTDATAREPORT","copyrightDataReportUUID":"1cfbdb47-cca7-3eca-b73e-0d6c478a5abc","isrc":"123Src","songTitle":"NY NY","startDate":"2018-01-01T21:17:34.371Z","endDate":"2018-11-15T22:27:34.111Z","rightHolders":[{"ipi":"ipi1","percent":42},{"ipi":"ipi2","percent":33}]}`
+var updatedCopyrightDateReportSingleInput = `[{"copyrightDataReportUUID":"1cfbdb47-cca7-3eca-b73e-0d6c478a5abc","docType":"COPYRIGHTDATAREPORT","isrc":"1234567Src","songTitle":"modified","startDate":"2018-01-01T21:17:34.371Z","endDate":"2018-11-15T22:27:34.111Z","rightHolders":[{"ipi":"ipi1","percent":42},{"ipi":"ipi2","percent":33}]}]`
 
 func MockGetCopyrightDataReportResponse(functionName string) []byte {
 	switch functionName {
@@ -26,6 +27,9 @@ func MockGetCopyrightDataReportResponse(functionName string) []byte {
 
 func MockGetCopyrightDataReport(stub shim.ChaincodeStubInterface, queryString string) ([]string, error) {
 	return []string{`{"docType":"COPYRIGHTDATAREPORT","copyrightDataReportUUID":"1cfbdb47-cca7-3eca-b73e-0d6c478a5abc","isrc":"123Src","songTitle":"NY NY","startDate":"2018-01-01T21:17:34.371Z","endDate":"2018-11-15T22:27:34.111Z","rightHolders":[{"ipi":"ipi1","percent":42},{"ipi":"ipi2","percent":33}]}`}, nil
+}
+func MockGetUpdatedCopyrightDataReport(stub shim.ChaincodeStubInterface, queryString string) ([]string, error) {
+	return []string{`{"docType":"COPYRIGHTDATAREPORT","copyrightDataReportUUID":"1cfbdb47-cca7-3eca-b73e-0d6c478a5abc","isrc":"1234567Src","songTitle":"modified","startDate":"2018-01-01T21:17:34.371Z","endDate":"2018-11-15T22:27:34.111Z","rightHolders":[{"ipi":"ipi1","percent":42},{"ipi":"ipi2","percent":33}]}`}, nil
 }
 func Test_AddCopyrightDataReports_Single(t *testing.T) {
 	scc := new(AxispointChaincode)
@@ -129,4 +133,23 @@ func Test_updateCopyrightDataReportByIDs(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 
+	_, err = checkInvoke(t, stub, [][]byte{[]byte("updateCopyrightDataReports"), []byte(updatedCopyrightDateReportSingleInput)})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	getCopyrightDataReportForQueryString = MockGetUpdatedCopyrightDataReport
+	actualReport, err := checkInvoke(t, stub, [][]byte{[]byte("getCopyrightDataReportByIDs"), []byte(copyrightDataReportUUID)})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	expectedReports, err := MockGetUpdatedCopyrightDataReport(stub, "")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	//normalize []string to string
+	if !reflect.DeepEqual("["+expectedReports[0]+"]", string(actualReport)) {
+		t.Fatalf("Actual response is not equal to expected response")
+	}
 }
